98c3d1d81da858465aff22f6d092708ef70c1ff4
SepehrNamdar <sepehr_gsfc@yahoo.com>
2022-03-07 12:55:04 +0100

    Use the Anti-Corruption Layer to translate foreign terms and ensure that our code respect the Ubiquitous Language

--- Let Me Be Free/src/main/java/use_case/CandidateIdMissingException.java
+++
@@ -1,4 +0,0 @@
-package use_case;
-
-public class CandidateIdMissingException extends RuntimeException {
-}
--- Let Me Be Free/src/main/java/use_case/PlanInterview.java
+++ Let Me Be Free/src/main/java/use_case/PlanInterview.java
@@ -6,26 +6,26 @@ import java.util.List;
 
 public class PlanInterview {
 
-    private final RecruiterRepository recruiters;
+    private final ConsultantRepository consultants;
     private final RoomRepository rooms;
 
-    public PlanInterview(RecruiterRepository recruiters, RoomRepository rooms) {
-        this.recruiters = recruiters;
+    public PlanInterview(ConsultantRepository consultants, RoomRepository rooms) {
+        this.consultants = consultants;
         this.rooms = rooms;
     }
 
-    public Interview scheduleInterview(InterviewDate interviewDate, HRCandidate candidate) {
-        candidate.checkCandidate();
+    public Interview scheduleInterview(InterviewDate interviewDate, Profile profile) {
+        profile.checkProfile();
         interviewDate.checkInterviewDate();
 
-        List<HRRecruiter> hrRecruiters = recruiters.findAll();
-        HRRecruiter recruiter = candidate.findRecruiter(interviewDate, hrRecruiters);
-        recruiter.book(interviewDate);
+        List<Consultant> consultants = this.consultants.findAll();
+        Consultant consultant = profile.findConsultant(interviewDate, consultants);
+        consultant.book(interviewDate);
         Room bookedRoom = new Room(rooms.book(interviewDate));
 
         bookedRoom.checkRoom();
 
-        return new Interview(recruiter, candidate, interviewDate, bookedRoom);
+        return new Interview(consultant, profile, interviewDate, bookedRoom);
     }
 
 }
