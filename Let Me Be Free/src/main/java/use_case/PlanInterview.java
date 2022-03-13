9adc66fb3ef4e98c8f66ea972bba472b8c8f0b2d
SepehrNamdar <sepehr_gsfc@yahoo.com>
2022-03-13 04:12:58 +0100

    use specific exceptions

--- Let Me Be Free/src/main/java/use_case/PlanInterview.java
+++ Let Me Be Free/src/main/java/use_case/PlanInterview.java
@@ -1,7 +1,6 @@
 package use_case;
 
 import model.interview.*;
-import service.Verification;
 
 import java.util.List;
 
@@ -9,17 +8,17 @@ public class PlanInterview {
 
     private final ConsultantRepository consultants;
     private final RoomRepository rooms;
-    private final Verification verifier;
 
-    public PlanInterview(ConsultantRepository consultants, RoomRepository rooms, Verification verifier) {
+    public PlanInterview(ConsultantRepository consultants, RoomRepository rooms) {
         this.consultants = consultants;
         this.rooms = rooms;
-        this.verifier = verifier;
     }
 
     public Interview scheduleInterview(InterviewDate interviewDate, Profile profile) {
-        verifier.checkCandidate(profile.toCandidate());
-        verifier.checkInterviewDate(interviewDate.toLocalDate());
+        ProfileValidator profileValidator = new ProfileValidator();
+        profileValidator.check(profile);
+        InterviewDateValidator interviewDateValidator = new InterviewDateValidator();
+        interviewDateValidator.check(interviewDate);
 
         List<Consultant> consultants = this.consultants.findAll();
         Consultant consultant = profile.findConsultant(interviewDate, consultants);
