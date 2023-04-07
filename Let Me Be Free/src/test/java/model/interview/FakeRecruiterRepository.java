bb9cd2e2ea50270ecb27314f5236647cc79cbcb6
SepehrNamdar <sepehr_gsfc@yahoo.com>
2023-04-07 12:23:11 +0200

    Update dates

--- Let Me Be Free/src/test/java/model/interview/FakeRecruiterRepository.java
+++ Let Me Be Free/src/test/java/model/interview/FakeRecruiterRepository.java
@@ -43,8 +43,8 @@ public class FakeRecruiterRepository implements RecruiterRepository {
                                         .dateOfBirth(LocalDate.of(1956, 8, 29))
                                         .build()))
                         .availabilities(new ArrayList<>(List.of(
-                                LocalDate.of(2022, 12, 22),
-                                LocalDate.of(2022, 12, 20))))
+                                LocalDate.of(2023, 12, 22),
+                                LocalDate.of(2023, 12, 20))))
                         .skills(List.of("Java"))
                         .experience(5)
                         .iban("AL47 2121 1009 0000 0002 3569 87411")
@@ -59,8 +59,8 @@ public class FakeRecruiterRepository implements RecruiterRepository {
                         .address("Chapman 711-2880 Nulla St. Mankato Mississippi 96522")
                         .dateOfBirth(LocalDate.of(1995, 3, 3))
                         .availabilities(new ArrayList<>(List.of(
-                                LocalDate.of(2022, 12, 21),
-                                LocalDate.of(2022, 12, 22))))
+                                LocalDate.of(2023, 12, 21),
+                                LocalDate.of(2023, 12, 22))))
                         .skills(List.of("PHP", "Java"))
                         .build());
         recruiters.add(Recruiter.builder()
@@ -68,7 +68,7 @@ public class FakeRecruiterRepository implements RecruiterRepository {
                         .name("Jones")
                         .firstName("Sara")
                         .availabilities(new ArrayList<>(List.of(
-                                LocalDate.of(2022, 12, 19))))
+                                LocalDate.of(2023, 12, 19))))
                         .candidates(List.of(
                                 Candidate.builder()
                                         .id("654")
@@ -100,8 +100,8 @@ public class FakeRecruiterRepository implements RecruiterRepository {
         recruiters.add(Recruiter.builder()
                         .id("101").name("Steve").firstName("Emma")
                         .availabilities(new ArrayList<>(List.of(
-                                LocalDate.of(2022, 12, 19),
-                                LocalDate.of(2022, 12, 22))))
+                                LocalDate.of(2023, 12, 19),
+                                LocalDate.of(2023, 12, 22))))
                         .skills(List.of("PHP", "Java"))
                         .build());
     }
