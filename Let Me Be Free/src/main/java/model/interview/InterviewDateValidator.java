984f514b3ec807ba7011e4c6a9686a1db40719fa
SepehrNamdar <sepehr_gsfc@yahoo.com>
2023-04-07 13:23:17 +0200

    make a better design

--- Let Me Be Free/src/main/java/model/interview/InterviewDateValidator.java
+++ Let Me Be Free/src/main/java/model/interview/InterviewDateValidator.java
@@ -1,14 +1,11 @@
 package model.interview;
 
 import service.InterviewDateMissingException;
-import service.Verification;
-import service.Verifier;
 import use_case.InterviewDateIsPassedException;
 
-public class InterviewDateValidator {
+public class InterviewDateValidator extends Validator<InterviewDate> {
 
     public void check(InterviewDate interviewDate) {
-        Verification verifier = new Verifier();
         try {
             verifier.checkInterviewDate(interviewDate.toLocalDate());
         } catch (InterviewDateMissingException e) {
